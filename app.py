c3a85894e747050a9cdf137a0a4467cd95cbed83
singhrau <singhrau@gmail.com>
2024-01-10 15:01:07 +0530

    ygyufuydgd

--- app.py
+++ app.py
@@ -20,13 +20,13 @@ def prediction():
         with open('model.pkl','rb') as model_file:
             mlmodel = pickle.load(model_file)
         res = mlmodel.predict([[float(nitro),float(phos),float(kp),float(temp),float(hum),float(ph),float(rain)]])
-        return render_template("result.html",res=res)
+        return render_template("result.html",res=res[0])
     else:
         return render_template('prediction.html')
 
 
 
 if __name__=='__main__':
-    app.run()
+    app.run(host = '0.0.0.0',port = 5050)
 
 
